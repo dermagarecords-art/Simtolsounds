3c923cdf77d0fce1ecd2adc4c3c1ce1fec3bbb12
dermagarecords-art <dermagarecords@gmail.com>
2026-08-02 22:44:19 +0700

    Update Header.tsx

--- components/site/Header.tsx
+++ components/site/Header.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import { simtolSoundsTypeImage } from "@/data/brandAssets";
 
 type Props = { catalogueNumber: string; title: string };
 
 export function Header({ catalogueNumber, title }: Props) {
   return <header className="player-header">
     <Link href="/" className="wordmark"><Image src="/images/simtol-logo-transparent.png" alt="Simtol Sounds" width={220} height={266} priority /></Link>
-    <div><span>{catalogueNumber}</span><span> / </span><span>{title}</span></div>
+    <Image className="header-type" src={simtolSoundsTypeImage} alt="Simtol Sounds" width={2894} height={272} priority unoptimized />
+    <div className="header-release"><span>{catalogueNumber}</span><span> / </span><span>{title}</span></div>
   </header>;
 }
