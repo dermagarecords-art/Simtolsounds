9de41b9940091128928335e1877cd58800981944
dermagarecords-art <dermagarecords@gmail.com>
2026-08-02 21:54:20 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import { featuredRelease } from "@/data/releases";
 import { InteractiveReleasePlayer } from "@/components/release/InteractiveReleasePlayer";
 
+export const metadata = {
+  alternates: {
+    canonical: "https://simtolsounds.site/",
+  },
+};
+
 export default function HomePage() {
   return <InteractiveReleasePlayer release={featuredRelease} />;
 }
